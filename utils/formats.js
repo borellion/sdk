0d842d81370b3e8e4b91bbcaf4855f50e79c716e
limbofeather <elijah@borellion.com>
2026-05-21 13:42:21 -0400

    sdk: update the IAB formats with borellion defaults

--- utils/formats.js
+++ utils/formats.js
@@ -32,27 +32,27 @@ const formats = {
         width: 0.56,
         height: 1,
         style: {
-            'standard': `${assetsURL}/zesty-default-mobile-phone-interstitial.png`,
-            'minimal': `${assetsURL}/zesty-default-mobile-phone-interstitial.png`,
-            'transparent': `${assetsURL}/zesty-default-mobile-phone-interstitial.png`
+            'standard': `${assetsURL}/borellion-default-mobile-phone-interstitial.png`,
+            'minimal': `${assetsURL}/borellion-default-mobile-phone-interstitial.png`,
+            'transparent': `${assetsURL}/borellion-default-mobile-phone-interstitial.png`
         }
     },
     'billboard': {
         width: 3.88,
         height: 1,
         style: {
-            'standard': `${assetsURL}/zesty-default-billboard.png`,
-            'minimal': `${assetsURL}/zesty-default-billboard.png`,
-            'transparent': `${assetsURL}/zesty-default-billboard.png`
+            'standard': `${assetsURL}/borellion-default-billboard.png`,
+            'minimal': `${assetsURL}/borellion-default-billboard.png`,
+            'transparent': `${assetsURL}/borellion-default-billboard.png`
         }
     },
     'medium-rectangle': {
         width: 1.2,
         height: 1,
         style: {
-            'standard': `${assetsURL}/zesty-default-medium-rectangle.png`,
-            'minimal': `${assetsURL}/zesty-default-medium-rectangle.png`,
-            'transparent': `${assetsURL}/zesty-default-medium-rectangle.png`
+            'standard': `${assetsURL}/borellion-default-medium-rectangle.png`,
+            'minimal': `${assetsURL}/borellion-default-medium-rectangle.png`,
+            'transparent': `${assetsURL}/borellion-default-medium-rectangle.png`
         }
     }
 }
