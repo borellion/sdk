9f3d2ee57c58a5b9e83a747258ef51dee2e90c31
limbofeather <elijah@borellion.com>
2026-05-21 11:10:46 -0400

    sdk(unity): update to borellion api url

--- unity/Assets/ZestySDK/Scripts/Internal/Utils/Constants.cs
+++ unity/Assets/ZestySDK/Scripts/Internal/Utils/Constants.cs
@@ -5,7 +5,7 @@
         public const string BEACON_URL = "https://beacon.zesty.market/api/v1";
         public const string BEACON2_URL = "https://beacon2.zesty.market/zgraphql";
         public const string AD_SERVER_URL = "https://api.borellion.com/api";
-        public const string CDN_URL = "https://cdn.zesty.xyz";
+        public const string CDN_URL = "https://cdn.borellion.com";
         public const bool PREBID = true;
         public const int MAX_PREBID_RETRIES = 4;
         public const int PREBID_REFRESH_INTERVAL = 10; // 10 seconds
